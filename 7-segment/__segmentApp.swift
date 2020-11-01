745ebc3f31b854ea6223132f66deb1faaf24d681
Damian Rzeszot <damian.rzeszot@gmail.com>
2020-11-01 22:43:28 +0100

    Initial Commit

---
+++ 7-segment/__segmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  __segmentApp.swift
+//  7-segment
+//
+//  Created by Damian Rzeszot on 01/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct __segmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
